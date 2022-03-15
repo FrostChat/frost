8dbf38a0d1c74c97e0d911ef1c68c0104d7ca961
Ben Tettmar <bentettmar@yahoo.com>
2022-03-15 19:46:19 +0000

    database changes

--- db-test.py
+++ db-test.py
@@ -2,18 +2,18 @@ from utils.sqlite import Database, UserExists
 
 db = Database()
 
-# username = input("Username: ")
-# password = input("Password: ")
+username = input("Username: ")
+password = input("Password: ")
 
-# db.add_user(username, password)
-# print("User added.")
+db.add_user(username, password)
+print("User added.")
 
-jim = db.get_user_by_username("jim")
-bob = db.get_user_by_username("bob")
+# jim = db.get_user_by_username("jim")
+# bob = db.get_user_by_username("bob")
 
-try:
-    db.mute_user(jim.id, bob.id)
-except Exception as e:
-    print(e)
+# try:
+#     db.mute_user(jim.id, bob.id)
+# except Exception as e:
+#     print(e)
 
-print(db.is_user_muted(jim.id, bob.id))
+# print(db.is_user_muted(jim.id, bob.id))
