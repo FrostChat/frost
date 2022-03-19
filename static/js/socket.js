a0ccf67ea598e7091221829a0aa17ab349b8986e
Ben Tettmar <bentettmar@yahoo.com>
2022-03-19 10:50:19 +0000

    Added alerts for system messages.

--- static/js/socket.js
+++ static/js/socket.js
@@ -6,35 +6,39 @@ socket.on('disconnected', () => { console.log('Disconnected from server') });
 socket.on('authenticated', () => { console.log('Authenticated') });
 
 socket.on('message', function(data) {
-    try {
-        if (user.id != self_user.id) {
-            messages.push(data);
-            add_message_div(data);
-
-            if (messages.length == 1) {
-                // check if user.id is not in users
-                let user_obj = users.find(x => x.id == user.id);
-                if (user_obj == undefined) {
-                    location.reload()
-                }
-            }
-        }
-    } catch {}
-
-    if (!document.hasFocus() || data.author_id != user.id) {
+    if (data.author_id == "system") {
+        alert(data.error);
+    } else {
         try {
-            if (data.receiver_id != user.id && data.author_id != self_user.id) {
-                if (!user.is_muted) {
-                    let audio = new Audio('/static/audio/notification.mp3');
-                    audio.play();
+            if (user.id != self_user.id) {
+                messages.push(data);
+                add_message_div(data);
+    
+                if (messages.length == 1) {
+                    // check if user.id is not in users
+                    let user_obj = users.find(x => x.id == user.id);
+                    if (user_obj == undefined) {
+                        location.reload()
+                    }
                 }
             }
         } catch {}
-        if (user.username == "duduser") {
-            user = users.find(x => x.id == data.author_id);
-        }
-        if (!user.is_muted) {
-            add_notification(data.author_id);
+    
+        if (!document.hasFocus() || data.author_id != user.id) {
+            try {
+                if (data.receiver_id != user.id && data.author_id != self_user.id) {
+                    if (!user.is_muted) {
+                        let audio = new Audio('/static/audio/notification.mp3');
+                        audio.play();
+                    }
+                }
+            } catch {}
+            if (user.username == "duduser") {
+                user = users.find(x => x.id == data.author_id);
+            }
+            if (!user.is_muted) {
+                add_notification(data.author_id);
+            }
         }
     }
 });
