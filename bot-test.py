1f7fd00a910a09b838c8367ce896e993c259291a
Ben Tettmar <bentettmar@yahoo.com>
2022-03-25 19:12:23 +0000

    Made a bot class.

--- bot-test.py
+++ bot-test.py
@@ -1,23 +1,52 @@
 import socketio
+import shlex
 
-sio = socketio.Client()
-sio.connect("http://localhost:8080", transports=["websocket"])
-sio.emit("authenticate", {"api_key": "Ym90.U3h2aHpnbFNzZUVrbW93WQ=="})
+class Message:
+    def __init__(self, id, content, author_id, timestamp):
+        self.id = id
+        self.content = content
+        self.author_id = author_id
+        self.timestamp = timestamp
 
-@sio.on('authenticated')
+class Bot:
+    def __init__(self):
+        self.sio = socketio.Client()
+
+    def run(self, api_key: str):
+        self.sio.connect('http://localhost:8080', transports=["websocket"])
+        self.sio.emit("authenticate", {"api_key": "Ym90.U3h2aHpnbFNzZUVrbW93WQ=="})
+
+    def event(self, *args):
+        event_name = args[0].__name__
+        event_callback = args[0]
+
+        self.sio.on(event_name, event_callback)
+
+frost_bot = Bot()
+
+@frost_bot.event
 def authenticated(data):
-    print("connected")
+    print("Connected to Frost")
 
-@sio.on('message')
+@frost_bot.event
 def message(data):
-    content = data.get("content")
-    author_id = data.get("author_id")
+    msg = Message(data["id"], data["content"], data["author_id"], data["timestamp"])
 
-    if content.startswith("!"):
-        command = content.replace("!", "").split(" ")
-        if command[0] == "ping":
-            sio.emit("send_message", {"content": """asdasd
-            asdad
-            asd
-            asdasd
-            """, "receiver_id": author_id})
+    if msg.content.startswith("/"):
+        cmd = msg.content[1:]
+        args = shlex.split(cmd)
+
+        if args[0] == "ping":
+            frost_bot.sio.emit("send_message", {"content": "pong", "receiver_id": msg.author_id})
+
+        if args[0] == "help":
+            help_message = """**📖 Commands**
+/ping - Pong
+/help - This message
+/info - Get info about Frost
+/userinfo [user_id] - Get info about a user
+/messageinfo [message_id] - Get info about a message
+"""
+            frost_bot.sio.emit("send_message", {"content": help_message, "receiver_id": msg.author_id})
+
+frost_bot.run("Qm9i.WllkdTdqWWtSMnpPUjVkRw==")
